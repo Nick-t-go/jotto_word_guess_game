498d35d9e85143843d887eb8577cef5c80dd2f07
nick-t-go <nickt444@gmail.com>
2019-04-04 12:02:47 -0400

    setting up guessWords tests"

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const actionTypes = {
 	CORRECT_GUESS: 'CORRECT_GUESS',
+	GUESS_WORD: 'GUESS_WORD'
 };
 
 /**
@@ -7,7 +8,13 @@ export const actionTypes = {
 * @returns {object} - Action object with type 'CORRECT_GUESS'
 */
 
-export function correctGuess() {
-	return { type: actionTypes.CORRECT_GUESS};
+// export function correctGuess() {
+// 	return { type: actionTypes.CORRECT_GUESS};
+// }
+
+export const guessWord = (guessedWord) => {
+	return function(dispatch, getState){
+
+	}
 }
 
---
+++ src/integration.test.js
@@ -0,0 +1,22 @@
+import { storeFactoyr } from '../test/testUtils';
+import { guessWord } from './actions';
+
+describe('guessWord action dispatcher', () => {
+	describe('no guessed words', () => {
+		test('updates state correctly for unsuccesful guess', () => {
+
+		})
+		test('updates state correctly for succesful guess', () => {
+			
+		})
+	})
+	describe('some guessed words', () => {
+		test('updates state correctly for unsuccesful guess', () => {
+
+		})
+		test('updates state correctly for succesful guess', () => {
+			
+		})
+	})
+});
+
