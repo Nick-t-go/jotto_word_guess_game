f70e27cad20d12665e587d33d584ab49979a7e71
nick-t-go <nickt444@gmail.com>
2019-04-03 23:28:27 -0400

    made store factory, downgraded react-redux

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,5 +1,11 @@
 import checkPropTypes from 'check-prop-types';
+import { createStore } from 'redux';
 
+import rootReducer from '../src/reducers';
+
+export const storeFactory = (initialState) => {
+	return createStore(rootReducer, initialState)
+}
 /**
   * @param {ShallowWrapper} wrapper  - Enzyme shallow wrapper to search withing
   * @param {string} val - calue of data-test attribute for search
